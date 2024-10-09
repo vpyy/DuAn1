c26c71e64ec86ab334acf097c63178ed8e7f976d
vpyy <phucpro170305@gmail.com>
2024-10-09 11:43:38 +0700

    Add files via upload

---
+++ 10a.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <stdio.h>
+int main()
+{
+  int n, i; 
+  long S;
+  S = 0;
+  printf("Nhap vao n: ");
+  scanf("%d", &n);
+  for(int i = 1; i <=n; i++)
+		S=S+pow(-1,n-1)*n
+  printf("Tong 1 - 2 + 3 - 4 ... %d la %ld: ", n, S);
+}
